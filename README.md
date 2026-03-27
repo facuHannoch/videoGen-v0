181c185836756c27fc08148c7e5046a7414a3afc
facu <facuhannoch@gmail.com>
2026-03-27 16:16:53 -0300

    README and things

--- README.md
+++ README.md
@@ -5,3 +5,73 @@ Goal: produce information and value-contained videos in an scalable and automate
 This could provide a low-intensity but steady source of traffic and authority for the channel. If the videos are correctly produced, then the information could still be provided, and thus the videos be valuable to people.
 
 Flow:
+
+1. Initial information or raw resources, such as content, idea definition, prompts for generation, etc, is generated.
+    - Components involved. `scriptGen`: takes a prompt file, allows string interpolation
+    - Resources used:
+        - prompt
+    - Generated resources: `content.json`
+2. Resources are created. 
+    - Components involved: `resourcesGen`
+    - Generated resources: `generated.resources.json`
+3. Resources are organized and used to build the script
+    - Components involved: `audioScriptGen`
+    - Resources used:
+        - xml reference file
+    - Generated resources: `script.xml`
+4. The audio is generated. Components involved: `audioGen`
+    - Components involved: `scriptGen`
+    - Generated resources: 
+        - `audios/`
+        - `audio.info.json`
+5. Video edition process: We need a way to make use of the generated resources, but also a way to render and edit the video. There are at least 3 steps: 1) bring back resources; 2) make editing; 3) render video (TBI)
+    - Components involved: 
+        - `videoEditorWorker`: The AI part of this step. Takes the resources and modifies the editor files themselves. Note: Currently this component its just a script that overrides the given file. To make this more inteliggent and allow it to further create / modify, it should trascend this script-state.
+        - `remotion-video-editor`: library to actually make the video and eventually render it. Uses the `remotion` library.
+    - Resources used
+        1. content.json
+        2. generated.resources.json
+        3. script.xml
+        4. audios/, audio.info.json
+        - video editing context: a file the videoEditorWorker has as additional context or instructions to provide the videoEditorWorker
+    - Generated resources: `video.out`
+
+Subsequent steps (not implemented, and not a priority)
+6. render video (needs UI or a way to easily do HITM if necessary)
+7. Submit video
+    1. To Storage service
+    2. To target platform / platforms
+8. Analytics
+
+Common resources: As the goal is to produce videos in an standard format, this is very suitable for series of videos. So a single prompt and reference file will be suitable for a full series of videos.
+
+Organization
+
+`projects`: Directory that contains the generated resources. Its structure is as follows:
+```
+/
+    CATEGORY/
+        _common/
+            promptXX.txt
+            reference-script-XXX.xml
+            video-making-context.txt
+        SPECIFIC_CONTENT/
+            en/
+                1-raw-content/
+                2-resources/
+                3-script/
+                4-audios/
+                5-videos/
+```
+
+This means that the library works always on a project, and each 
+
+
+
+- `bashis.sh`: shell script that executes this in sequence.
+
+
+---
+
+All steps make use of AI to allow for flexible and idea-guided videos, instead of following a strict process.
+This will mean that the process is not 100% deterministic. Specially in step 2, this could imply the need for a HITM approach, where the generation can be easily reviewed and then the user could approve / reject the resources generated.
--- bashis.sh
+++ bashis.sh
@@ -1,98 +1,3 @@
-
-
-
-
-
-
-
-<<'COMMENT'
-1. Obtain details 
-2. Generate script
-
-COMMENT
-
-
-
-########################
-
-# load env variables
-
-
-# python3 scriptGen/index.py -o scriptGen/res.json --template scriptGen/prompt.txt --map PHONEME_HERE=g
-
-# python3 resourcesGen/index.py --input scriptGen/res.json --assets-dir resourcesGen/out --output resourcesGen/generated.resources.json
-
-# python3 audioScriptGen/index.py --input scriptGen/res.json --reference audioScriptGen/reference.xml --output audioScriptGen/script.xml
-
-######################################
-# Generate audio
-# Takes the script, generates the audio by pieces and a unified audio metadata, and copies that to public/audios in the editor
-
-
-# ---
-# Remove previous audios, and move the current ones to a -prev directory
-
-# rm -r $projectPath/audios-prev
-# rm $projectPath/audio-prev.info.json
-
-# mv $projectPath/audios $projectPath/audios-prev
-# mv $projectPath/audio.info.json $projectPath/audio-prev.info.json
-
-# # ---
-# # Generate the audios
-
-# python3 audioGen/index.py --xml $projectPath/script.xml --output-dir $projectPath/audios --unify-json
-# mv $projectPath/audios/audio.info.json $projectPath/audio.info.json
-
-
-# #######################################
-# # Video editor
-
-# # Remove existing audios (maybe unnecessary) and copy new generated resources
-
-# rm -r $videoEditorAssetsDir/audios
-# rm $videoEditorAssetsDir/audio.info.json
-
-# cp -r $projectPath/audios $videoEditorAssetsDir/audios
-# cp $projectPath/audio.info.json $videoEditorAssetsDir/audio.info.json
-
-
-# mv images $videoEditorAssetsDir/diagram.png
-
-
-
-
-#
-#
-#
-#
-#
-#
-#
-#
-
-#
-
-
-######################################
-
-# 1. Create g directory in PROJECT/g/
-# 2. Move prompt into g/1/ or use a common prompt, 
-#     i. Pass prompt as cli argument
-#     ii. Move prompt into g/1-raw-content/prompt.txt
-#     iii. Use prompt a level above, like PROJECT/_common/prompt.txt
-#     # iv. Use prompt in scriptGen/prompt.txt
-# 3. ??
-# ??
-# ?. Move assets to remotion-video-editor. Rename image to diagram.
-
-
-# This assumes the script runs with the root in videoGen
-
-# content=ʃ
-# lang=es
-# category=ipa_coach-phonemes
-
 category=ipa_coach-word_pronunciation
 content=bed
 lang=es
@@ -100,8 +5,6 @@ lang=es
 projectPath=public/$category/$content/$lang
 videoEditorAssetsDir=remotion-video-editor/public
 
-# initialPromptPath=
-# scriptReferencePath=
 
 mkdir -p $projectPath
 mkdir $projectPath/1-raw-content
@@ -109,26 +12,24 @@ mkdir $projectPath/2-resources
 mkdir $projectPath/3-script
 mkdir $projectPath/4-audios
 
-# xmlReferencePath=$projectPath/2-resources/script-reference.xml
-
 promptPath=public/$category/_common/prompt-$lang.txt
 xmlReferencePath=public/$category/_common/reference-$lang.xml
 videoEditionContext=public/$category/_common/videoMakingContext.txt
 
 
-# echo " ----- $content ----- "
-# echo "--- \n Step 1 - Raw content creation \n---"
+echo " ----- $content ----- "
+echo "--- \n Step 1 - Raw content creation \n---"
 
-# python3 scriptGen/index.py -o $projectPath/1-raw-content/content.json --template $promptPath --map PHONEME_HERE=$content
+python3 scriptGen/index.py -o $projectPath/1-raw-content/content.json --template $promptPath --map PHONEME_HERE=$content
 
-# echo "\n--- \n Step 2 - Resources creation \n---"
+echo "\n--- \n Step 2 - Resources creation \n---"
 
-# python3 resourcesGen/index.py --input $projectPath/1-raw-content/content.json --assets-dir $projectPath/2-resources/images/ --output $projectPath/2-resources/generated.resources.json --image-aspect-ratio 1:1 
+python3 resourcesGen/index.py --input $projectPath/1-raw-content/content.json --assets-dir $projectPath/2-resources/images/ --output $projectPath/2-resources/generated.resources.json --image-aspect-ratio 1:1 
 
 
-# echo "\n--- \n Step 3 - Audio Script creationg\n---"
+echo "\n--- \n Step 3 - Audio Script creationg\n---"
 
-# python3 audioScriptGen/index.py --input $projectPath/1-raw-content/content.json --reference $xmlReferencePath --output $projectPath/3-script/script.xml --google-model gemini-3-flash-preview
+python3 audioScriptGen/index.py --input $projectPath/1-raw-content/content.json --reference $xmlReferencePath --output $projectPath/3-script/script.xml --google-model gemini-3-flash-preview
 
 
 ######################################
@@ -167,7 +68,6 @@ cp -r $projectPath/4-audios/audios $videoEditorAssetsDir/audios
 cp $projectPath/4-audios/audio.info.json $videoEditorAssetsDir/audio.info.json
 
 cp -r $projectPath/2-resources/images $videoEditorAssetsDir/
-exit
 
 
 # Video editor - video edition
@@ -196,4 +96,4 @@ python3 videoEditorWorker/index.py --composition remotion-video-editor/src/Compo
 # Analytics
 
 # 1. Register video in ??
-# 2. Periodically check it
+# 2. Periodically check it -> write learnings within the specific project directory
